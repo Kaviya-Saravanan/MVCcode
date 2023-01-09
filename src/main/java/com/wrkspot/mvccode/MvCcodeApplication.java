34ce48c8cc3098ac7246543e9f73bbbb818d41a1
Kaviya-Saravanan <kaviya.saravanan@wrkspot.com>
2023-01-09 21:42:38 +0530

    Initial code

---
+++ src/main/java/com/wrkspot/mvccode/MvCcodeApplication.java
@@ -0,0 +1,28 @@
+package com.wrkspot.mvccode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCcodeApplication {
+
+    public static void main(String[] args) {
+        StudentModelViewController model=Database();
+        StudentView view=new StudentView();
+        StudentController controller=new StudentController(model,view);
+        controller.Update();
+        controller.setStudentName("Mary");
+        System.out.println("Updated");
+        controller.Update();
+
+    }
+    public static StudentModelViewController Database(){
+        StudentModelViewController student=new StudentModelViewController();
+        student.setName("Alisa");
+        student.setAge(21);
+        student.setAverage(30.3);
+        student.setGrade('C');
+        return student;
+    }
+
+}
---
+++ src/test/java/com/wrkspot/mvccode/MvCcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrkspot.mvccode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvCcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
